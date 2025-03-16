06410e32b947eb37cb80d6e440da2eecf7e8d552
EddieWjy <3040953216@qq.com>
2025-03-16 23:03:53 +0800

    📃 docs: 更新项目信息，修复Markdown编辑器的banner路径，优化日志内容

--- docs/.vitepress/userConfig/projectsInfo.ts
+++ docs/.vitepress/userConfig/projectsInfo.ts
@@ -19,7 +19,7 @@ export const projectsInfo: Project[] = [
     tag: "React",
   },
   {
-    banner: "/project-img/markdown.png", //
+    banner: "/project-img/markdown.png",
     title: "IMarkdown",
     description: "基于原生TS自主设计解析器的Markdown编辑器,支持扩展语法",
     link: "https://github.com/Eddiewjy/md-parser",
